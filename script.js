a59bf398c3ee2056dee6b8c19068b08e9093e601
obZZen <andy540@gmail.com>
2022-10-15 10:44:39 -0600

    final update

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+const request = new XMLHttpRequest();
+  request.open("GET", "navigation.json", false);
+  request.send(null);
+const data  = JSON.parse(request.responseText);
+
 const nav   = document.getElementById('main-nav');
 const line  = document.getElementById('line');
 const list  = [document.getElementById('main-nav').getElementsByTagName('a')];
-const data  = JSON.parse('{"cities":[{"section":"cupertino","label":"Cupertino"},{"section":"new-york-city","label":"New York City"},{"section":"london","label":"London"},{"section":"amsterdam","label":"Amsterdam"},{"section":"tokyo","label":"Tokyo"},{"section":"hong-kong","label":"Hong Kong"},{"section":"sydney","label":"Sydney"}]}');
+let timeUpdate;
 
 //generate Nav based on JSON data
 function makeNav(){
    for (var i = 0; i < data.cities.length; i++) {
 
-      var city = data.cities[i];
-      var li = document.createElement('li');
-      var a  = document.createElement('a');
+      let city = data.cities[i];
+      let li = document.createElement('li');
+      let a  = document.createElement('a');
 
-      a.id = city.section;
-        a.appendChild(document.createTextNode(city.label));
+        a.id = city.section; // set id
+        a.setAttribute('timezone', city.timezone); // set timezone
+        a.appendChild(document.createTextNode(city.label)); // set label text
         li.appendChild(a);
         nav.appendChild(li);
    }
@@ -20,8 +26,8 @@ function makeNav(){
 
 //function to move line, and check active state, active clock
 function lineMove(e){
-  var theID = document.getElementById(e.srcElement.id);
-  var positionInfo = e.srcElement.getBoundingClientRect();
+  let theID = document.getElementById(e.srcElement.id);
+  let positionInfo = e.srcElement.getBoundingClientRect();
 
   //check and set active states
   for (var i = 0; i < list[0].length; i++){
@@ -46,7 +52,7 @@ function lineMove(e){
 
 //re-calibrate line, based on which nav item is active
 function recalculateOnResize(){
-  var findActive = document.getElementsByClassName('active');
+  let findActive = document.getElementsByClassName('active');
   if(findActive){
     positionInfo = findActive[0].getBoundingClientRect();
     line.style.width = positionInfo.width +'px';
@@ -54,18 +60,48 @@ function recalculateOnResize(){
   }
 }
 
-// I know this is nuts, I was trying to stay fully vanilla JS and don't know how to do this
-// without an API and library. I could have hard coded the times and offset the UTC,
-// then just switched clocks on and off according to the 'active' nav item. But that
-// feels bad too, because I am getting the city names dynamically, and should be fed into an API.
-function stealTime(e){
-  var theID = document.getElementById(e.srcElement.id);
-  var timeString = theID.innerHTML;
-  var url = new URL('https://time.is/'+ timeString);
 
-  document.getElementById('clock-iframe').src = url;
+// grab timezone attribute from clicked
+function resetTime(e){
+   let theID = document.getElementById(e.srcElement.id);
+   let zone  = theID.getAttribute('timezone');
+   let localTime = getTimeByZone(zone);
 }
 
+
+
+function getTimeByZone(timeZone){
+    let now = new Date(Date.now());
+    let printTime = document.getElementById('clock');
+    let printDate = document.getElementById('date');
+
+    if(now != null){
+      timeUpdate = setInterval(function(){
+        if(timeZone != null){
+          let now = new Date(Date.now());
+          let month = now.toLocaleDateString('en-US',{timeZone,month:'long'}).substring(0,3);
+          let localDateString =  now.toLocaleDateString('en-US',{timeZone});
+
+          let date = new Date(localDateString).getDate();
+          let year = new Date(localDateString).getFullYear();
+          let customTimeString = date + ' ' + month + ', '+ year;
+
+          let time = now.toLocaleTimeString('en-US',{timeZone,hour12:false});
+
+          printTime.innerHTML = time;
+          printDate.innerHTML = customTimeString;
+
+          return {date:customTimeString,time}
+
+      } else {
+        //reset clock if clicked off city
+        printTime.innerHTML = '';
+        printDate.innerHTML = '';
+      }
+    },1000)
+  }
+
+}
 
 
 //init
@@ -74,7 +110,8 @@ const init = () =>{
 
   nav.addEventListener('click', (e) => {
     lineMove(e);
-    stealTime(e);
+    clearInterval(timeUpdate);
+    resetTime(e);
   });
 
   onresize = () => {
